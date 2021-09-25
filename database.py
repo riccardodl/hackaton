4ec1c3a98dbff2f4613b10d4a76403aac41a4bfa
Riccardo <riccardodl92@gmail.com>
2021-09-25 09:27:09 +0200

    removed bug, cleaned up a little bit

---
+++ database.py
@@ -0,0 +1,34 @@
+import json
+import csv
+
+class Database(object):
+    def __init__(self):
+        super().__init__()
+
+    def get_next_id(self):
+        with open('database.csv', newline='') as file:
+            data = csv.reader(file)
+            count = len(list(data))
+            return count
+
+
+    def get_entry(self, drug_name):
+        with open('database.csv', newline='') as file:
+            data = csv.reader(file)
+            for row in data:
+                #elem = row[0].split(',', 4)
+                if row[1] == drug_name:
+                    return row[3]
+    
+            return 'Entry not found'
+
+    def put_entry(self, barcode_id, drug_name, barcode_type, pdf):
+        json = Database.pdf_to_json(pdf)
+        with open('database.csv', 'a+', newline='') as file:
+            writer = csv.writer(file)            
+            writer.writerow([barcode_id,drug_name,barcode_type,json])
+        return None
+
+    @classmethod
+    def pdf_to_json(self, pdf):
+        return 'json'
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask import request
-import json
-import csv
+from .database import Database
 
 app = Flask(__name__)
 
+#TODO get string and a qr code type, pull pdf form db, import module from raul that processes the pdf and return a json string
 
 @app.route("/get/barcode/<barcode>", methods=['GET'])
 def get_json(barcode):
@@ -16,7 +16,7 @@ def get_json(barcode):
 @app.route("/put/<drug>", methods=['PUT'])
 def put_json(drug):
     qr_code = request.form['qr_code']
-    #pdf = request.files['pdf']
+    #pdf = request.files['pdf'] uncomment once we send a pdf file in the request
 
     db_handler = Database()
     id = db_handler.get_next_id()
@@ -24,42 +24,5 @@ def put_json(drug):
     return 'Success'
 
 
-#TODO get string and a qr code type, pull pdf form db, import module from raul that processes the pdf and return a json string
-
-
-class Database(object):
-    def __init__(self):
-        super().__init__()
-
-    def get_next_id(self):
-        with open('database.csv', newline='') as file:
-            data = csv.reader(file)
-            count = len(list(data))
-            return count
-
-
-    def get_entry(self, drug_name):
-        with open('database.csv', newline='') as file:
-            data = csv.reader(file)
-            for row in data:
-                elem = row[0].split(',', 4)
-                if elem[1] == drug_name:
-                    return elem[3]
-                else:
-                    break         
-    
-            return 'Entry not found'
-
-    def put_entry(self, barcode_id, drug_name, barcode_type, pdf):
-        json = Database.pdf_to_json(pdf)
-        with open('database.csv', 'w+', newline='') as file:
-            writer = csv.writer(file)
-            row = f'{barcode_id},{drug_name},{barcode_type},{json}'
-            writer.writerow([row])
-        return None
-
-    @classmethod
-    def pdf_to_json(self, pdf):
-        return 'json'
 
 
