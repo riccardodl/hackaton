ab2a36b8511c7789c009d1d2e553f40e0953f4e9
Riccardo <riccardodl92@gmail.com>
2021-09-25 22:33:12 +0200

    add static

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import request
 from shared_code.database import Database
 from shared_code.pdf_parser import *
-import json
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def get_json(barcode):
     db_handler = Database()
     data = db_handler.get_entry(barcode)
-    return data
+    return render_template('content.html')#, data=data)
 
 #HTML forms must use enctype=multipart/form-data or files will not be uploaded.
 @app.route("/put/<drug>", methods=['PUT'])
